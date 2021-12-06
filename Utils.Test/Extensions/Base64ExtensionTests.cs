d0227bc37051744af11259852894165066647cb6
Johannes Tegnér <johannes@jitesoft.com>
2021-12-06 19:36:38 +0100

    Base solution and test. Simple b64 extension created.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

---
+++ Utils.Test/Extensions/Base64ExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿using Jitesoft.Utils.Extensions;
+using Xunit;
+
+namespace Jitesoft.Utils.Test.Extensions
+{
+    public class Base64ExtensionTests
+    {
+        [Theory]
+        [InlineData("dGVzdA", "dGVzdA==")]
+        [InlineData("dGVz", "dGVz")]
+        [InlineData("dGVzdGluZw", "dGVzdGluZw==")]
+        public void TestAddPadding(string input, string expected)
+        {
+            Assert.Equal(expected, input.AddBase64Padding());
+        }
+
+        [Theory]
+        [InlineData("dGVzdA==", "dGVzdA")]
+        [InlineData("dGVz", "dGVz")]
+        [InlineData("dGVzdGluZw==", "dGVzdGluZw")]
+        public void TestRemovePadding(string input, string expected)
+        {
+            Assert.Equal(expected, input.RemoveBase64Padding());
+        }
+    }
+}
---
+++ Utils/Extensions/Base64Extension.cs
@@ -0,0 +1,31 @@
+﻿namespace Jitesoft.Utils.Extensions
+{
+    public static class Base64Extension
+    {
+        /// <summary>
+        /// Pads a base64 encoded string with `=`.
+        /// </summary>
+        /// <param name="self">String to modify.</param>
+        /// <returns>String with padding.</returns>
+        public static string AddBase64Padding(this string self)
+        {
+            while (self.Length % 4 != 0)
+            {
+                self += '=';
+            }
+
+            return self;
+        }
+
+        /// <summary>
+        /// Remove padding from base64 encoded strings.
+        /// </summary>
+        /// <param name="self">String to modify.</param>
+        /// <returns>String without padding.</returns>
+        public static string RemoveBase64Padding(this string self)
+        {
+            self = self.TrimEnd('=');
+            return self;
+        }
+    }
+}
